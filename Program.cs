13f365f33759966550ca8bd0837ceb8a92514037
Matis <matis@matis.io>
2025-04-02 20:31:03 -0300

    Agregamos la configuracion para sesiones.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSession(); 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
+
+app.UseSession();
 
 app.UseRouting();
 
