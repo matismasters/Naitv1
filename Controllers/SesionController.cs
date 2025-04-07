33920535e1be7c1be96de8113256820b37abca39
Matis <matis@matis.io>
2025-04-07 20:04:36 -0300

    Terminamos con las sesiones

--- Controllers/SesionController.cs
+++ Controllers/SesionController.cs
@@ -25,9 +25,23 @@ namespace Naitv1.Controllers
         [HttpPost]
         public IActionResult Iniciar(string email, string password)
         {
-            HttpContext.Session.SetString("estaLogueado", "true");
-            HttpContext.Session.SetString("nombreUsuario", email);
-            return Redirect("/");
+            List<Usuario> resultado = _context.Usuarios.Where(
+                usuario => usuario.Email == email && usuario.Password == MD5Libreria.Encriptar(password)
+            ).ToList();
+
+            if (resultado.Count > 0)
+            {
+                Usuario usuario = resultado.First();
+
+                HttpContext.Session.SetString("estaLogueado", "true");
+                HttpContext.Session.SetString("nombreUsuario", usuario.Nombre);
+                HttpContext.Session.SetString("emailUsuario", usuario.Email);
+
+                return Redirect("/");
+            } else
+            {
+                return Redirect("/Sesion/ErrorDeInicio");
+            }
         }
 
         public IActionResult Registrarse()
@@ -35,22 +49,51 @@ namespace Naitv1.Controllers
             return View();
         }
 
+        public IActionResult ErrorDeRegistro()
+        {
+            return View();
+        }
+
+        public IActionResult ErrorDeInicio()
+        {
+            return View();
+        }
+
+        public IActionResult CuentaCreadaConExito()
+        {
+            ViewBag.NombreUsuario = HttpContext.Session.GetString("nombreUsuario") ?? "";
+
+            return View();
+        }
+        
+        public IActionResult Salir()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/");
+        }
+
         [HttpPost]
         public IActionResult CrearUsuario(string nombre, string email, string password, string passwordConfirmation)
         {
-            Usuario usuario = new Usuario();
-            usuario.Email = email;
-            usuario.Nombre = nombre;
-            usuario.Password = MD5Libreria.Encriptar(password);
+            if ( password == passwordConfirmation)
+            {
+                Usuario usuario = new Usuario();
+                usuario.Email = email;
+                usuario.Nombre = nombre;
+                usuario.Password = MD5Libreria.Encriptar(password);
 
-            _context.Usuarios.Add(usuario);
-            HttpContext.Session.SetString("estaLogueado", "true");
-            HttpContext.Session.SetString("nombreUsuario", nombre);
-            HttpContext.Session.SetString("emailUsuario", email);
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges();
 
-            _context.SaveChanges();
+                HttpContext.Session.SetString("estaLogueado", "true");
+                HttpContext.Session.SetString("nombreUsuario", nombre);
+                HttpContext.Session.SetString("emailUsuario", email);
 
-            return Redirect("/");
+                return Redirect("/Sesion/CuentaCreadaConExito");
+            } else
+            {
+                return Redirect("/Sesion/ErrorDeRegistro");
+            }
         }
     }
 }
