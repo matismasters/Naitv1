95afcf6357dd327bd495de7f6137f97d366ce9ae
IgnacioNegro <negroignacio2014@gmail.com>
2025-04-28 19:24:45 -0300

    todo ok

---
+++ Controllers/PartnersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Naitv1.Data;
+using Naitv1.Helpers;
+using Naitv1.Models;
+
+namespace Naitv1.Controllers
+{
+    public class PartnersController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public PartnersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public IActionResult Index(string nombre, string? direccion, string logoUrl, string? descripcion,int telefono, string email)
+        {
+            Usuario usuario = UsuarioLogueado.Usuario(HttpContext.Session);
+            Partner partner = new Partner();
+
+            partner.Nombre = nombre;
+            partner.Direccion = direccion;
+            partner.LogoUrl = logoUrl;
+            partner.Descripcion = descripcion;
+            partner.Telefono = telefono;
+            partner.Email = email;
+            partner.Estado = EstadoPartner.Pendiente;
+            _context.Partners.Add(partner);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+       
+    }
+}
---
+++ Migrations/20250428220947_actualizado.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Naitv1.Migrations
+{
+    /// <inheritdoc />
+    public partial class actualizado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstadoPartner",
+                table: "Partners");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Partners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Estado",
+                table: "Partners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Telefono",
+                table: "Partners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Partners");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Partners");
+
+            migrationBuilder.DropColumn(
+                name: "Telefono",
+                table: "Partners");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EstadoPartner",
+                table: "Partners",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
