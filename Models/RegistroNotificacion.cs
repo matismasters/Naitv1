4ba176e78d187ba0caf4593ec6c165f088ca6ce5
Matis <matis@matis.io>
2025-05-05 21:50:55 -0300

    Vista Notificaciones, Servicio de Moderacion, clase RegistroNotificaciones modelo

---
+++ Models/RegistroNotificacion.cs
@@ -0,0 +1,18 @@
+﻿namespace Naitv1.Models
+{
+    public class RegistroNotificacion
+    {
+        public int RegistroId { get; set; }
+
+        public int ReferenciaId { get; set; }
+
+        public string Tipo { get; set; }
+
+        public string Motivo { get; set; }
+
+        public string EstadoNotificacion { get; set; }
+
+        public DateTime FechaNotificacion { get; set; } = DateTime.Now;
+
+    }
+}
