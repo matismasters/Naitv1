8b53ce9df0249aa562005c829f4540e6ea12bec2
Matis <matis@matis.io>
2025-05-07 19:05:47 -0300

    roto sessiones

--- Controllers/SesionController.cs
+++ Controllers/SesionController.cs
@@ -111,6 +111,7 @@ namespace Naitv1.Controllers
                 usuario.Email = email;
                 usuario.Nombre = nombre;
                 usuario.Password = MD5Libreria.Encriptar(password);
+                usuario.TipoUsuario = "basico";
 
 				_context.Usuarios.Add(usuario);
                 _context.SaveChanges();
@@ -122,6 +123,71 @@ namespace Naitv1.Controllers
             {
                 return Redirect("/Sesion/ErrorDeRegistro");
             }
+        }
+
+<<<<<<< HEAD
+        public IActionResult CrearUsuarioAnfitrion(string nombre, string email, string password, string passwordConfirmation)
+=======
+        [HttpPost]
+        public IActionResult CrearUsuarioAnfitrion(string nombre, string email, string password, string passwordConfirmation)
+		{
+			if (UsuarioLogueado.estaLogueado(HttpContext.Session))
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			if (password == passwordConfirmation)
+			{
+				Usuario usuario = new Usuario();
+				usuario.Email = email;
+				usuario.Nombre = nombre;
+				usuario.Password = MD5Libreria.Encriptar(password);
+				usuario.TipoUsuario = "Anfitrion";
+
+				_context.Usuarios.Add(usuario);
+				_context.SaveChanges();
+
+				UsuarioLogueado.loguearUsuario(HttpContext.Session, usuario);
+
+				return Redirect("/Sesion/CuentaCreadaConExito");
+			}
+			else
+			{
+				return Redirect("/Sesion/ErrorDeRegistro");
+			}
+		}
+
+        public IActionResult RegistroAnfitrion()
+>>>>>>> siete-mayo
+        {
+            if (UsuarioLogueado.estaLogueado(HttpContext.Session))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+<<<<<<< HEAD
+            if (password == passwordConfirmation)
+            {
+                Usuario usuario = new Usuario();
+                usuario.Email = email;
+                usuario.Nombre = nombre;
+                usuario.Password = MD5Libreria.Encriptar(password);
+                usuario.TipoUsuario = "anfitrion";
+
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges();
+
+                UsuarioLogueado.loguearUsuario(HttpContext.Session, usuario);
+
+                return Redirect("/Sesion/CuentaCreadaConExito");
+            }
+            else
+            {
+                return Redirect("/Sesion/ErrorDeRegistro");
+            }
+=======
+            return View();
+>>>>>>> siete-mayo
         }
     }
 }
