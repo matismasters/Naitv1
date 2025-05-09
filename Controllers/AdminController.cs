29037d22f232ea82c7f88dd5b02acd85e028598f
Juan Lluviera <juanlluviera03@gmail.com>
2025-05-09 00:01:54 -0300

    Creacion de modelos, vistas y controllers necesarios para la EPIC. Creacion de migraciones correspondientes

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Naitv1.Data;
+
+namespace Naitv1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Notificaciones()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Notificaciones.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Naitv1.Models
+{
+    public class Notificaciones
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public DateTime FechaHoraProgramada { get; set; }
+
+        public string CriterioSegmento { get; set; }
+
+        public string EstadoNotificacion { get; set; }
+
+    }
+}
