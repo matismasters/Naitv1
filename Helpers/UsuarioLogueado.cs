1259567094fd3099ac777edf9c9c5c880780ffa3
jcar512 <209171620+jcar5@users.noreply.github.com>
2025-05-10 18:54:25 -0300

    agregado chequeo para usuario moderador

--- Helpers/UsuarioLogueado.cs
+++ Helpers/UsuarioLogueado.cs
@@ -2,51 +2,57 @@
 using Naitv1.Models;
 
 namespace Naitv1.Helpers
-{
-    public class UsuarioLogueado
     {
-
-        public static bool estaLogueado(ISession sesionActual)
+    public class UsuarioLogueado
         {
+        public static bool estaLogueado(ISession sesionActual)
+            {
             string estaLogueadoString = sesionActual.GetString("estaLogueado") ?? "false";
             bool estaLogueado = estaLogueadoString == "true";
 
             return estaLogueado;
-        }
+            }
 
         public static bool esAnfitrion(ISession sesionActual)
-        {
+            {
             string tipoUsuarioString = sesionActual.GetString("tipoUsuario") ?? "basico";
             bool esAnfitrion = tipoUsuarioString == "anfitrion";
 
             return esAnfitrion;
-        }
+            }
 
         public static string nombreUsuario(ISession sessionActual)
-        {
+            {
             string nombreUsuario = sessionActual.GetString("nombreUsuario") ?? "";
             return nombreUsuario;
-        }
+            }
 
         public static bool esSuperAdmin(ISession sessionActual)
-        {
+            {
             string tipoUsuarioString = sessionActual.GetString("tipoUsuario") ?? "basico";
             bool esSuperAdmin = tipoUsuarioString == "superadmin";
             return esSuperAdmin;
-        }
+            }
+
+        public static bool esModerador(ISession sessionActual)
+            {
+            string tipoUsuarioString = sessionActual.GetString("tipoUsuario") ?? "basico";
+            bool esModerador = tipoUsuarioString == "moderador";
+            return esModerador;
+            }
 
         public static void loguearUsuario(ISession sesionActual, Usuario usuario)
-        {
+            {
             sesionActual.SetString("estaLogueado", "true");
 
             sesionActual.SetInt32("idUsuario", usuario.Id);
             sesionActual.SetString("nombreUsuario", usuario.Nombre);
             sesionActual.SetString("emailUsuario", usuario.Email);
             sesionActual.SetString("tipoUsuario", usuario.TipoUsuario);
-        }
+            }
 
         public static Usuario Usuario(ISession sesionActual)
-        {
+            {
             Usuario usuario = new Usuario();
             usuario.Id = sesionActual.GetInt32("idUsuario") ?? 0;
             usuario.Nombre = sesionActual.GetString("nombreUsuario") ?? "";
@@ -54,6 +60,6 @@ namespace Naitv1.Helpers
             usuario.TipoUsuario = sesionActual.GetString("tipoUsuario") ?? "basico";
 
             return usuario;
+            }
         }
     }
-}
