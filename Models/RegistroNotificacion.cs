12dc0c425a8efde417b86718c2caaa92e7be19de
Norax4 <angenoramurteza001@gmail.com>
2025-05-13 16:22:34 -0300

    Inicio de sesion como moderador, UsuarioId en RegistroNotificacion, Views de Moderacion, cambios en Layout

--- Models/RegistroNotificacion.cs
+++ Models/RegistroNotificacion.cs
@@ -6,12 +6,23 @@
 
         public int ReferenciaId { get; set; }
 
+        public int UsuarioId { get; set; }
+
+		public Actividad? ActividadRef { get; set; }
+
         public string Tipo { get; set; } = "Actividad";
 
         public string Motivo { get; set; }
 
-        public string EstadoNotificacion { get; set; }
+        public string Descripcion { get; set; }
+
+		public string EstadoNotificacion { get; set; }
 
         public DateTime FechaNotificacion { get; set; } = DateTime.Now;
+
+        public List<string> Motivos = new List<string>
+        {
+            "Peligroso", "Fraude", "Actividad Politica", "Engañoso", "Otros"
+        };
         }
     }
