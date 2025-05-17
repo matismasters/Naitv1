ec3eb9256a89a48de9347da238302d94fef547cb
Gabriel <gabriell-3@hotmail.com>
2025-05-16 23:31:32 -0300

    Se agrego comentario en ReporteController

---
+++ Naitv1/Services/GeneradorReportesService.cs
@@ -0,0 +1,60 @@
+﻿using Naitv1.Data;
+using Naitv1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Naitv1.Services
+{
+    public class GeneradorReportesService
+    {
+        private readonly AppDbContext _context;
+
+        public GeneradorReportesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CrearRegistro(DateTime fechaProgramada)
+        {
+            string html = GenerarHtmlConReporte();
+
+            var registro = new RegistroEmail
+            {
+                Destinatario = "kpis@empresa.com",
+                Asunto = "Resumen Semanal de KPIs",
+                CuerpoHtml = html,
+                FechaProgramada = fechaProgramada,
+                Estado = "Pendiente"
+            };
+
+            _context.RegistroEmails.Add(registro);
+            _context.SaveChanges();
+        }
+
+        public string GenerarHtmlConReporte()
+        {
+            string rutaImagen = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "graficos", "graficoKpis.png");
+
+            // Leemos la imagen como base64
+            byte[] imagenBytes = File.ReadAllBytes(rutaImagen);
+            string base64Imagen = Convert.ToBase64String(imagenBytes);
+
+            return $@"
+            <html>
+            <head><meta charset='utf-8'></head>
+            <body>
+                <h2>Reporte Semanal</h2>
+                <ul>
+                    <li><b>Usuarios nuevos:</b> 124</li>
+                    <li><b>Ventas:</b> $3,200</li>
+                    <li><b>Actividades creadas:</b> 57</li>
+                </ul>
+                <h3>Gráfico:</h3>
+                <img src='data:image/png;base64,{base64Imagen}' width='400' height='200' />
+            </body>
+            </html>";
+        }
+
+    }
+}
