ead0df4fbb66275debf461086ae661ec33b4c77d
Matis <matis@matis.io>
2025-06-04 21:34:06 -0300

    Agregar decoradores para marcadores de actividades. Utilizando patron Factory y Decorator

---
+++ Models/ActividadMarcadorFilosofar.cs
@@ -0,0 +1,42 @@
+﻿namespace Naitv1.Models
+{
+    public class ActividadMarcadorFilosofar : IMarcadorDeMapa
+    {
+        private readonly Actividad _actividad;
+
+        public ActividadMarcadorFilosofar(Actividad actividad)
+        {
+            _actividad = actividad;
+        }
+
+        public float lat()
+        {
+            return _actividad.Lat;
+        }
+
+        public float lon()
+        {
+            return _actividad.Lon;
+        }
+
+        public string mensajeDelAnfitrion()
+        {
+            return "Filosofando: " + _actividad.MensajeDelAnfitrion;
+        }
+
+        public string urlImagenMarcador()
+        {
+            return "/assets/marcadorFilosofar.png";
+        }
+
+        public string tipoActividad()
+        {
+            return "Filosofar!";
+        }
+
+        public int idActividad()
+        {
+            return _actividad.Id;
+        }
+    }
+}
