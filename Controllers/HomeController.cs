ab17fda7d7201401518cd334704ff94120291eac
Norax4 <angenoramurteza001@gmail.com>
2025-06-06 22:37:36 -0300

    Modal y funciones para reportar actividades

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -72,6 +72,7 @@ namespace Naitv1.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Controllers/RegistroReporteController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Naitv1.Data;
+using Naitv1.Helpers;
+using Naitv1.Models;
+
+namespace Naitv1.Controllers
+{
+    public class RegistroReporteController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public RegistroReporteController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult ReportarActividad(int idActividad, string motivo, string descripcion)
+        {
+            if (UsuarioLogueado.estaLogueado(HttpContext.Session) == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Usuario usuario = UsuarioLogueado.Usuario(HttpContext.Session);
+            RegistroNotificacion notificacion = _context.RegistroNotificaciones
+                .Where(a => a.UsuarioId == usuario.Id)
+                .Where(a => a.ActividadId == idActividad)
+                .FirstOrDefault();
+
+            if (notificacion != null)
+            {
+                return Json(new { error = "Ya hay un reporte creado" });
+            }
+
+            RegistroNotificacion nuevoRegistro = new RegistroNotificacion
+            {
+                ActividadId = idActividad,
+                UsuarioId = usuario.Id,
+                Motivo = motivo,
+                Descripcion = descripcion
+            };
+
+            _context.RegistroNotificaciones.Add(nuevoRegistro);
+            _context.SaveChanges();
+
+            return Json(nuevoRegistro.Id);
+        }
+    }
+}
